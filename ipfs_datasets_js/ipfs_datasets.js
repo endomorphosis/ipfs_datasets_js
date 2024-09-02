498d7be8bbd05f51c496f2df009e32e3cbda7f89
endomorphosis <starworks5@gmail.com>
2024-09-01 17:49:36 -0700

    update

--- ipfs_datasets_js/ipfs_datasets.js
+++ ipfs_datasets_js/ipfs_datasets.js
@@ -2,7 +2,7 @@ import { PGlite } from "@electric-sql/pglite";
 
 export class ipfsDatasetsJs {
     constructor(resources, metadata) {
-        // this._contract = new Contract("ipfs_accelerate", ipfs_accelerate_abi);
+
     }
 
     async init(libp2p_kit_js, orbitdb_kit_js, ipfs_kit_js) {
@@ -27,17 +27,5 @@ export class ipfsDatasetsJs {
         return this.pglite.query(sql);
     }
 
-    async getContract() {
-        return this._contract;
-    }
-
-    async getIpfsHash() {
-        return await this._contract.methods.getIpfsHash().call();
-    }
-
-    async setIpfsHash(ipfsHash) {
-        return await this._contract.methods.setIpfsHash(ipfsHash).send();
-    }
-
 }
-export default ipfsAccelerateJs;
+export default ipfsDatasetsJs;

86b8a254c9a6e7182ea7375abfcf3558b7b69298
endomorphosis <starworks5@gmail.com>
2024-09-01 18:15:00 -0700

    update

--- ipfs_datasets_js/index.js
+++ ipfs_datasets_js/index.js
@@ -1,2 +1,2 @@
-export { ipfsAccelerateJs } from './ipfs_accelerate_js/ipfs_accelerate.js';
-export { ipfsAccelerateJs as default } from './ipfs_accelerate_js/ipfs_accelerate.js';
+export { ipfsDatasetsJs } from './ipfs_accelerate_js/ipfs_accelerate.js';
+export { ipfsDatasetsJs as default } from './ipfs_accelerate_js/ipfs_accelerate.js';

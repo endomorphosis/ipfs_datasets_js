3d5a56c6527726e646104f67950c964ee5d7fbaa
endomorphosis <starworks5@gmail.com>
2024-09-04 17:09:12 -0700

    chore: Update import path for ipfsDatasetsJs in test.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ export { ipfsDatasetsJs } from './ipfs_datasets_js/ipfs_datasets.js';
 export { ipfsDatasetsJs as default } from './ipfs_datasets_js/ipfs_datasets.js';
 export * from './test/test.js';
 export { test_ipfs_datasets_js } from './test/test.js';
+import { test_ipfs_datasets_js } from './test/test.js';
 const test = new test_ipfs_datasets_js();
 export { test };
